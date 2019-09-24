71f3f095f6c13ef2f2bb79bce6500ae0c493973a
LAPTOP-A096FB15\jakub <jakub.porubcansky@tangent.works>
2019-09-24 21:07:32 +0200

    wip

--- resources/main.js
+++ resources/main.js
@@ -35,13 +35,19 @@
 // var toProjection = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
 // var extent = new OpenLayers.Bounds(-1.32,51.71,-1.18,51.80).transform(fromProjection,toProjection);
 
+var data = {"0" : {name: "name0", imgUrl: "resources/cinema0.jpg", longitude:-0.1279688, latitude:51.5077286}, 
+            "1" : {name: "name1", imgUrl: "resources/cinema1.jpg", longitude:-0.1280000, latitude:51.5050000},
+            "2" : {name: "name2", imgUrl: "resources/cinema2.jpg", longitude:-0.1290000, latitude:51.5060000}
+}
+
+
 function markerOnClick(id) {
     id_int = parseInt(id)
 
     if(level > 0)
     {   
         confirmButton.disabled = false;
-        textSelection.innerHTML = "You have selected cinema with id: ".concat(id)
+        textSelection.innerHTML = "You have selected ".concat(data[id].name).concat(" cinema")
 
         map.layers[1].markers[id_int].icon.setUrl("resources/marker_active.png")
 
@@ -55,42 +61,34 @@ function markerOnClick(id) {
     }
 }
 
-/////////////////////////////////////////////////////////////////////////////////
-
 map = new OpenLayers.Map("mapdiv");
 map.addLayer(new OpenLayers.Layer.OSM());
-
-var lonLat = new OpenLayers.LonLat( -0.1279688 ,51.5077286 )
-    .transform(
-        new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
-        map.getProjectionObject() // to Spherical Mercator Projection
-    );
-var lonLat2 = new OpenLayers.LonLat( -0.1280000 ,51.5050000 )
-    .transform(
-        new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
-        map.getProjectionObject() // to Spherical Mercator Projection
-    );    
     
 var zoom = 16;
 
 var markers = new OpenLayers.Layer.Markers("Markers");
 map.addLayer(markers);
 
-var marker = new OpenLayers.Marker(lonLat, new OpenLayers.Icon('resources/marker_inactive.png', new OpenLayers.Size(50,50)));
-marker.id = "0";
-marker.events.register("mousedown", marker, function() {
-    markerOnClick(this.id);
-});
+for(var key in data){
+    var lonLat = new OpenLayers.LonLat(data[key].longitude, data[key].latitude)
+    .transform(
+        new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
+        map.getProjectionObject() // to Spherical Mercator Projection
+    );
+    var marker = new OpenLayers.Marker(lonLat, new OpenLayers.Icon('resources/marker_inactive.png', new OpenLayers.Size(50,50)));
+    marker.id = key;
+    marker.events.register("mousedown", marker, function() {
+        markerOnClick(this.id);
+    });
+    markers.addMarker(marker);
+}
 
-var marker2 = new OpenLayers.Marker(lonLat2, new OpenLayers.Icon('resources/marker_inactive.png', new OpenLayers.Size(50,50)));
-marker2.id = "1";
-marker2.events.register("mousedown", marker2, function() {
-    markerOnClick(this.id);
-});
-
-markers.addMarker(marker);
-markers.addMarker(marker2);
-map.setCenter(lonLat2, zoom);  
+var center = new OpenLayers.LonLat(data["0"].longitude, data["0"].latitude)
+    .transform(
+        new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
+        map.getProjectionObject() // to Spherical Mercator Projection
+    );
+map.setCenter(center, zoom);  
 
 //////////////////////////////////////////////////////////////////////////////////
 
@@ -108,11 +106,8 @@ var level = 0
 startButton.addEventListener('click', (event) => {
     startPage.style.display = 'none';
     quizPage.style.display = 'block';
+    nickname = document.getElementById("inputNickname").value
     level ++;
 });
 
 
-
-// markers[0].events.register("mousedown", marker, function() {
-//     alert(this.id);
-// });
